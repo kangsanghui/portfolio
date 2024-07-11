9ad99e9ece50bc1bab4db46255147ca59ca1af41
kangsanghui <99458925+kangsanghui@users.noreply.github.com>
2024-07-12 03:45:52 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head id="hd">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description"  content="" />
+    <meta name="keywords" content="">
+    <link rel="canonical" href="" />
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="/img/main/logo.png" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+    <meta name="twitter:card" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="/img/main/logo.png" />
+    <meta itemprop="image" content="/img/main/logo.png" />
+    <link rel="icon" type="image/png" sizes="180x180"  href="/img/main/favicon.png">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.4.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+</head>
+<body>
+    <header id="hd" data-include-path="header.html">
+
+    </header>
+    <footer id="ft"></footer>
+
+    <script>
+        $(document).ready(function () {
+            $('#hd').load("/header.html");
+        });
+    </script>
+</body>
+</html>
